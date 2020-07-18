06e0c059b6ac3b953b50c1bbb89d16fc62e02ad6
Your Name <you@example.com>
2020-07-18 22:38:27 +0600

    is array work(check who the array)

---
+++ is_array.php
@@ -0,0 +1,14 @@
+
+<?php
+
+$refat = "refat";
+echo "check array" .is_array($refat) . "<br>";
+
+$tanvir = array("goodBoy","he have a girl friend");
+echo "tanvir is" .is_array($tanvir) . "<br>";
+
+$jubair = array("ThickBoy","he have't girl friend");
+echo "jubair is" .is_array($jubair) ."<br>";
+
+$komal = array("badBoy","he have noithing");
+echo "komal is" .is_array($komal)."<br>";

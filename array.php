3e97fbde0a0890b4ffbdb1f00704ede922ed2281
Your Name <you@example.com>
2020-07-18 22:17:11 +0600

    first php array function class

---
+++ array.php
@@ -0,0 +1,6 @@
+
+    <?php
+
+
+     $pent=array("full-pent","half-pent","short-pent","jainga");
+     echo "I like " . $pent[0] . "but my family dont like" . $pent[1] . " and i dont use " . $pent[2] . "rajib er gpre";

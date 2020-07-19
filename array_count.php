5a14e2d74bf3aaba1d121541eef35a229da8543c
Your Name <you@example.com>
2020-07-19 18:11:10 +0600

    array_count

---
+++ array_count.php
@@ -0,0 +1,41 @@
+<?php 
+$company = [
+    'Google',
+    'Microsoft',
+    'Apple',
+    'Adobe',
+    'Cisco',
+    'Juniper',
+    'Lenovo',
+    'Samsung'
+    
+];
+$employee
+=[
+    'refat',
+    'tanvir',
+    'ashik',
+    'jubair',
+    'komol'
+       
+];
+
+$product
+
+=[
+    'movile',
+    'tv',
+    'table',
+    'pen',
+    'laptoop',
+    'remove',
+    'Lenovo',
+    'remote'
+    
+];
+
+$c1 = count($product);
+$c2 = count($employee);
+$c3 = count($company);
+
+echo "is This total $c1 company total employee $c2 total product $c3";
